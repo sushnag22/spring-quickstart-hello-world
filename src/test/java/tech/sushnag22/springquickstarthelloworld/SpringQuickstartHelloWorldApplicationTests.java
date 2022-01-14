0f4634134da1fd2b1bbe575585f032fc8d65ed92
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-01-14 10:55:37 +0530

    Initial commit

---
+++ src/test/java/tech/sushnag22/springquickstarthelloworld/SpringQuickstartHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.springquickstarthelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickstartHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
